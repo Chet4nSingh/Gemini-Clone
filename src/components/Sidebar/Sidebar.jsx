ac881ac52d50fbc63fa5e27938cfce74b57b669a
Chetan Singh <chet4nsingh@gmail.com>
2024-09-23 23:06:36 +0530

    Gemini Clone

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,9 +11,27 @@ function Sidebar() {
                 <img src={assets.plus_icon} alt="" />
                 <p>New Chat</p>
             </div>
+            <div className="recent">
+                <p className="recent-title"></p>
+                <div className="recent-entry">
+                    <img src={assets.message_icon} alt="" />
+                    <p>What is next???</p>
+                </div>
+            </div>
         </div>
         <div className="bottom">
-
+            <div className="bottom-item recent-entry">
+                <img src={assets.question_icon} alt="" />
+                <p>Help</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.history_icon} alt="" />
+                <p>Activity</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.setting_icon} alt="" />
+                <p>Settings</p>
+            </div>
         </div>
     </div>
   )
